03bf65a645a6a11874b5c31f677748d3666ee1b5
Gitravs <kitravee.swk@gmail.com>
2019-06-23 13:51:08 +0700

    add main.c USART commuication

--- src/main.c
+++ src/main.c
@@ -8,7 +8,9 @@
   ******************************************************************************
 */
 
-
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
 #include "stm32f4xx.h"
 
 #include "FreeRTOS.h"
@@ -21,13 +23,43 @@ TaskHandle_t xTaskHandle2 = NULL;
 void vTask1_handler(void *params);
 void vTask2_handler(void *params);
 
+#ifdef USE_SEMIHOSTING
+	extern void initialise_monitor_handles();
+#endif
+
+static void prvSetupHardware(void);
+static void prvSetupUart(void);
+
+//some macros
+#define TRUE 1
+#define FALSE 0
+#define AVAILABLE TRUE
+#define NOT_AVAILABLE FALSE
+
+void printmsg(char *msg);
+
+char usr_msg[250]={0};
+uint8_t UART_ACCESS_KEY = AVAILABLE;
+
+
 int main(void)
 {	/*PUSH*/
+
+#ifdef USE_SEMIHOSTING
+	initialise_monitor_handles();
+	printf("this is hello world");
+#endif
+
 	RCC_DeInit();
 	SystemCoreClockUpdate();
+	prvSetupHardware();
+
+	sprintf(usr_msg,"This is hello-world application starting\r\n");
+	printmsg(usr_msg);
+
+
 	xTaskCreate(vTask1_handler,"Task1", configMINIMAL_STACK_SIZE, NULL, 2, &xTaskHandle1);
 	xTaskCreate(vTask2_handler,"Task2", configMINIMAL_STACK_SIZE, NULL, 2, &xTaskHandle2);
-
 	vTaskStartScheduler();
 
 	for(;;);
@@ -35,11 +67,72 @@ int main(void)
 
 void vTask1_handler(void *params)
 {
-	while(1);
+	while(1){
+		if(UART_ACCESS_KEY == AVAILABLE)
+		{
+			UART_ACCESS_KEY = NOT_AVAILABLE;
+			printmsg("Hello-world: From Task-1\r\n");
+			UART_ACCESS_KEY = AVAILABLE;
+			taskYIELD();
+		}
+	}
 }
 
 void vTask2_handler(void *params)
 {
-	while(1);
+	while(1){
+		if(UART_ACCESS_KEY == AVAILABLE)
+		{
+			UART_ACCESS_KEY = NOT_AVAILABLE;
+			printmsg("Hello-world: From Task-2\r\n");
+			UART_ACCESS_KEY = AVAILABLE;
+			taskYIELD();
+		}
+	}
+}
+
+static void prvSetupUart(void)
+{
+	GPIO_InitTypeDef gpio_uart_pins;
+	USART_InitTypeDef uart2_init;
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2, ENABLE);
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
+	gpio_uart_pins.GPIO_Pin = GPIO_Pin_2 | GPIO_Pin_3;
+	gpio_uart_pins.GPIO_Mode = GPIO_Mode_AF;
+	gpio_uart_pins.GPIO_PuPd = GPIO_PuPd_UP;
+	gpio_uart_pins.GPIO_OType= GPIO_OType_PP;
+	gpio_uart_pins.GPIO_Speed = GPIO_High_Speed;
+
+	GPIO_Init(GPIOA, &gpio_uart_pins);
+
+	GPIO_PinAFConfig(GPIOA, GPIO_PinSource2, GPIO_AF_USART2); //PA2
+	GPIO_PinAFConfig(GPIOA, GPIO_PinSource3, GPIO_AF_USART3); //PA3
+
+	memset(&uart2_init, 0, sizeof(uart2_init));
+	uart2_init.USART_BaudRate = 115200;
+	uart2_init.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+	uart2_init.USART_Mode = USART_Mode_Tx | USART_Mode_Rx;
+	uart2_init.USART_Parity = USART_Parity_No;
+	uart2_init.USART_StopBits = USART_StopBits_1;
+	uart2_init.USART_WordLength = USART_WordLength_8b;
+	USART_Init(USART2, &uart2_init);
+
+	USART_Cmd(USART2, ENABLE);
+}
+
+static void prvSetupHardware(void)
+{
+	prvSetupUart();
+}
+
+void printmsg(char *msg)
+{
+	for(uint32_t i=0; i < strlen(msg); i++)
+	{
+		while ( USART_GetFlagStatus(USART2,USART_FLAG_TXE) != SET);
+		USART_SendData(USART2,msg[i]);
+	}
+
+
 }
 
